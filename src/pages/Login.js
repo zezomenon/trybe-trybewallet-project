e1f410ad0f0383528369caf9962f70b0927fb079
Israel Menon <zezomenon@gmail.com>
2021-01-06 18:46:22 -0300

    Requisito 1 - Começando form de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,53 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailInput: '',
+      passwordInput: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { emailInput, passwordInput } = this.state;
+    return (
+      <div>
+        <form>
+          <label htmlFor="emailInput">
+            Email:
+            <input
+              type="email"
+              name="emailInput"
+              data-testid="email-input"
+              value={ emailInput }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="passwordInput">
+            Senha:
+            <input
+              type="password"
+              name="passwordInput"
+              data-testid="password-input"
+              value={ passwordInput }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
