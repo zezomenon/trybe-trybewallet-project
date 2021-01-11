f2cae36a5e111aa88294c1d14265e8aacd51d545
Israel Menon <zezomenon@gmail.com>
2021-01-11 11:18:00 -0300

    Iniciando reducer para wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET:
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default wallet;
