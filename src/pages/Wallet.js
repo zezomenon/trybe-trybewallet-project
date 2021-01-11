2a30d3ecad2ff954d23a675a6b9e562f7b9e464e
Israel Menon <zezomenon@gmail.com>
2021-01-11 11:17:18 -0300

    Criando campos pedidos no requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    const totalField = 0;
+    return (
+      <div>
+        <h2 data-testid="email-field">
+          Usuário:
+          { userEmail }
+        </h2>
+        <h3 data-testid="total-field">{ totalField }</h3>
+        <h3 data-testid="header-currency-field">BRL</h3>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
